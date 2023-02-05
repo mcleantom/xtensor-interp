984f5e0c65ab96b5231d193957ac4a646fecbef0
mcleantom <mcleantom97@gmail.com>
2023-02-05 11:53:47 +0000

    Implementing ND linear interpolation

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # XTensor-Interp
 
-XTensorInterp creates a class that allows the interpolation of N-Dimensional grids.
+XTensorInterp implements a lazy-evaluation N-Dimensional interpolator for regular-gridded data using the library
+xtensor.
 
-This class is designed to be similar to the Scipy RegularGridInterpolator, however it will scale better to higher-dimensional grids.
+You can set-up an interpolator class like:
 
-In addition, the library will allow for the interpolation for XArray Datasets, allowing for multiple datasets that are on identical grids to be efficiently interpolated.
+```cpp
+Regular<double> x_axis(0, 10, 20);
+auto y_data = xt::linspace<double>(-5, 5, 20);
+Interpolator<double> interp_1d(x_axis, y_data);
+```
+
+Then you can evaluate it by calling the class:
+```cpp
+auto x_in = xt::linspace<double>(0, 10, 100);
+xt::xarray<double> res = interp_1d(x_in);
+```
 
 ## Development
 
@@ -14,15 +25,12 @@ Remember to initialize the repository by initializing the submodules using the c
 git clone --recurse-submodules https://github.com/mcleantom/xtensor-interp.git
 ```
 
+It is recommended to develop this library with VSCode. The recommended extensions to develop this library will be recommended when you open the project.
+
 ### CMake
 
-This project uses CMake to configure the project. You must have at least version 3.20.
+This project uses CMake to configure the project and download it's dependencies. You must have at least version 3.20.
 
 ### GTest
 
-googletest is uses for unit testing the C++ library. 
-
-
-## Similar Projects
-
-This project is heavily influenced by [pangeo-pyinterp](https://github.com/CNES/pangeo-pyinterp).
+googletest is uses for unit testing the C++ library (and is installed using CMake).
--- XTensorInterp/XTensorInterp/interpolator.h
+++ XTensorInterp/XTensorInterp/interpolator.h
@@ -3,7 +3,9 @@
 
 #include "container.h"
 #include "axis.h"
+#include "itertools.h"
 #include <memory.h>
+#include <utility>
 #include <xtensor/xarray.hpp>
 
 template<typename T>
@@ -33,6 +35,67 @@ public:
         return res;
     };
 private:
+    T evaluate_linear(std::vector<size_t> indices, std::vector<T> norm_distances) {
+        assert(indices.size() == norm_distances.size());
+        /* The formula for linear interpolation in 2D is the form:
+            value = m_Data[i0, i1]      * (1-y0)    * (1-y1) +
+                    m_Data[i0, i1+1]    * (1-y0)    * y1 +
+                    m_Data[i0+1, i1]    * y0        * (1-y1) +
+                    m_Data[i0+1, i1+1]  * y0        * y1
+        
+            Notice how:
+                i_n maps to 1-y_n
+            and
+                i_n+1 maps to y_n
+        */
+
+        // First, pre-calculate 1-yn
+        size_t n = indices.size();
+
+        std::vector<T> shift_norm_distances;
+        std::transform(
+            norm_distances.begin(),
+            norm_distances.end(),
+            shift_norm_distances.begin(),
+            std::back_inserter(shift_norm_distances),
+            [](auto & x) { return 1-x; }
+        );
+
+        // And pre-calculate in + 1
+        std::vector<size_t> shift_indices;
+        std::transform(
+            indices.begin(),
+            indices.end(),
+            shift_indices.begin(),
+            std::back_inserter(shift_norm_distances),
+            [](auto & x) { return 1-x; }
+        );
+
+        // We pair in to (1 - yn)
+        std::vector<std::tuple<size_t, T>> zipped1(n);
+        for (size_t i=0; i<indices.size(); ++i) {
+            zipped1[i] = { indices[i], shift_norm_distances[i] };
+        };
+
+        // and we pair in + 1 to yn
+        std::vector<std::tuple<size_t, T>> zipped2(n);
+        for (size_t i=0; i<indices.size(); ++i) {
+            zipped2[i] = { shift_indices[i], norm_distances[i] };
+        };
+
+        // Now map (in, 1-yn) to (in+1, yn)
+        std::vector<std::tuple<std::tuple<size_t, T>>> zipped3(n);
+        for (size_t i=0; i<zipped1.size(); ++i) {
+            zipped3[i] = {zipped1[i], zipped2[i]};
+        }
+
+        // Take the cartesian product of zipped1 and zipped2
+        auto hypercube = product(zipped1, zipped2);
+        for (const auto[edge_indices, weights] : hypercube) {
+            
+        }
+    };
+
     Container<T>& m_x;
     xt::xarray<T> m_Data;
 };
