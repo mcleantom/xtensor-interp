5266b8857b21ff5a3def9ee2927fe737b77df19d
mcleantom <mcleantom97@gmail.com>
2023-02-06 22:40:52 +0000

    WIP Trilinear interpolation refactor

--- XTensorInterp/XTensorInterp/interpolator.h
+++ XTensorInterp/XTensorInterp/interpolator.h
@@ -4,6 +4,7 @@
 #include "container.h"
 #include "axis.h"
 #include "itertools.h"
+#include <limits>
 #include <memory.h>
 #include <utility>
 #include <xtensor/xarray.hpp>
@@ -18,14 +19,33 @@ public:
         xt::xarray<T> data
     )
     : m_Grid(std::move(grid))
-    , m_Data(std::move(data)) {};
+    , m_Data(std::move(data))
+    , m_NDIMS(m_Grid.size()) {};
+
+    /**
+     * @brief Interpolaton at coordinates
+     * 
+     * @param xarr xt::xarray of shape (NPOINTS, NDIMS)
+     * @return xt::xarray<T> ndarray of shape (NPOINTS, )
+     */
     xt::xarray<T> operator() (const xt::xarray<T>& xarr) {
         auto dims = xarr.shape();
+        if (dims[1] != m_NDIMS) {
+            throw std::invalid_argument("input vector has the wrong number of dimensions");
+        }
         auto res = xt::empty<T>(dims);
 
-        auto [indices, norm_distances] = find_indices(xarr);
+        auto [indices, norm_distances] = find_indices(xt::transpose(xarr));
 
-        assert(indices.shape()[0] == dims[0]);
+        assert(
+            xt::xarray<size_t>({{3, 7}, {15, 8}, {20, 1}}) == indices
+        );
+
+        assert(
+            xt::amax(xt::xarray<T>({{2.f/3.f, 2.f/3.f}, {0.4, 0.4}, {0.8, 0.6}}) - norm_distances)(0) < 1e-7
+        );
+        
+        auto res1 = evaluate_linear(indices, norm_distances);
 
         for (size_t i=0; i<dims[0]; ++i) {
             auto x = xarr[i];
@@ -50,10 +70,13 @@ private:
      points in xi.
      */
     std::tuple<xt::xarray<size_t>, xt::xarray<T>> find_indices(const xt::xarray<T>& xi) {
-        size_t I = m_Grid.size();
-        size_t J = xi.size();
-        xt::xarray<size_t> indices = xt::zeros<size_t>({I, J});
-        xt::xarray<T> norm_distances = xt::zeros<T>({I, J});
+        auto shape = xi.shape();
+        auto I = shape[0];
+        auto J = shape[1];
+        xt::xarray<size_t> indices = xt::zeros<size_t>(shape);
+        xt::xarray<T> norm_distances = xt::zeros<T>(shape);
+
+        std::cout << "xi: \n" << xi << std::endl;
 
         for (size_t i=0; i<I; ++i) {
             std::unique_ptr<Container<T>> &grid = m_Grid[i];
@@ -61,14 +84,20 @@ private:
             xt::xarray<T> nd = xt::zeros<size_t>({J});
 
             for (size_t j=0; j<J; ++j) {
-                auto idx = grid->findIndex(xi[i, j], false);
-                ix[j] = grid->findIndex(xi[i, j], false);
+                // std::vector<size_t> indexer = {i, j};
+                auto x = xi.at(i, j);
+                auto idx = grid->findIndex(x, false);
+                ix[j] = grid->findIndex(x, false);
                 auto x1 = grid->coordinateValue(idx);
                 auto x2 = grid->coordinateValue(idx+1);
                 auto dx = x2-x1;
-                auto norm_dist = (xi[i, j] - x1) / dx;
+                auto norm_dist = (x-x1)/dx;
                 nd[j] = norm_dist;
             }
+
+            std::cout << ix << std::endl;
+            std::cout << nd << std::endl;
+
             xt::view(indices, i, xt::all()) = ix;
             xt::view(norm_distances, i, xt::all()) = nd;
         }
@@ -76,9 +105,17 @@ private:
         std::tuple<xt::xarray<size_t>, xt::xarray<T>> rtn = {indices, norm_distances};
         return rtn;
     };
-
-    T evaluate_linear(std::vector<size_t>& indices, std::vector<T>& norm_distances) {
-        assert(indices.size() == norm_distances.size());
+    /**
+     * @brief Do linear interpolation.
+     * 
+     * @param indices an xarray of size_t of shape (NDIMS, NPOINTS)
+     * @param norm_distances an xarray of T of shape (NDIMS, NPOINTS)
+     * @return T 
+     */
+    T evaluate_linear(xt::xarray<size_t>& indices, xt::xarray<T>& norm_distances) {
+        assert(indices.shape() == norm_distances.shape());
+        assert(indices.shape()[0] == m_NDIMS);
+        assert(indices.shape().size() == 2);
         /* The formula for linear interpolation in 2D is the form:
             value = m_Data[i0, i1]      * (1-y0)    * (1-y1) +
                     m_Data[i0, i1+1]    * (1-y0)    * y1     +
@@ -91,54 +128,77 @@ private:
                 i_n+1 maps to y_n
         */
 
-        // First, pre-calculate 1-yn
         size_t n = indices.size();
 
-        std::vector<T> shift_norm_distances;
-        std::transform(
-            norm_distances.begin(),
-            norm_distances.end(),
-            shift_norm_distances.begin(),
-            std::back_inserter(shift_norm_distances),
-            [](auto & x) { return 1-x; }
-        );
+        xt::xarray<T> shift_norm_distances = 1 - norm_distances;    // (1-yn)
+        xt::xarray<T> shift_indices = indices + 1;                  // in + 1
 
-        // And pre-calculate in + 1
-        std::vector<size_t> shift_indices;
-        std::transform(
-            indices.begin(),
-            indices.end(),
-            shift_indices.begin(),
-            std::back_inserter(shift_norm_distances),
-            [](auto & x) { return 1-x; }
-        );
-
-        // We pair in to (1 - yn)
+        // pair in to 1-yn
         std::vector<std::tuple<size_t, T>> zipped1(n);
         for (size_t i=0; i<indices.size(); ++i) {
             zipped1[i] = { indices[i], shift_norm_distances[i] };
         };
 
-        // and we pair in + 1 to yn
+        // pair in+1 to yn
         std::vector<std::tuple<size_t, T>> zipped2(n);
         for (size_t i=0; i<indices.size(); ++i) {
             zipped2[i] = { shift_indices[i], norm_distances[i] };
         };
 
-        // Now map (in, 1-yn) to (in+1, yn)
-        std::vector<std::tuple<std::tuple<size_t, T>>> zipped3(n);
-        for (size_t i=0; i<zipped1.size(); ++i) {
-            zipped3[i] = {zipped1[i], zipped2[i]};
-        }
-
-        // Take the cartesian product of zipped1 and zipped2
         auto hypercube = product(zipped1, zipped2);
-        for (const auto[edge_indices, weights] : hypercube) {
+
+        std::cout << "hi2" << std::endl;
+
+
+
+
+        // std::vector<T> shift_norm_distances;
+        // std::transform(
+        //     norm_distances.begin(),
+        //     norm_distances.end(),
+        //     shift_norm_distances.begin(),
+        //     std::back_inserter(shift_norm_distances),
+        //     [](auto & x) { return 1-x; }
+        // );
+
+        // And pre-calculate in + 1
+        // std::vector<size_t> shift_indices;
+        // std::transform(
+        //     indices.begin(),
+        //     indices.end(),
+        //     shift_indices.begin(),
+        //     std::back_inserter(shift_norm_distances),
+        //     [](auto & x) { return 1-x; }
+        // );
+
+        // We pair in to (1 - yn)
+        // std::vector<std::tuple<size_t, T>> zipped1(n);
+        // for (size_t i=0; i<indices.size(); ++i) {
+        //     zipped1[i] = { indices[i], shift_norm_distances[i] };
+        // };
+
+        // // and we pair in + 1 to yn
+        // std::vector<std::tuple<size_t, T>> zipped2(n);
+        // for (size_t i=0; i<indices.size(); ++i) {
+        //     zipped2[i] = { shift_indices[i], norm_distances[i] };
+        // };
+
+        // // Now map (in, 1-yn) to (in+1, yn)
+        // std::vector<std::tuple<std::tuple<size_t, T>>> zipped3(n);
+        // for (size_t i=0; i<zipped1.size(); ++i) {
+        //     zipped3[i] = {zipped1[i], zipped2[i]};
+        // }
+
+        // // Take the cartesian product of zipped1 and zipped2
+        // auto hypercube = product(zipped1, zipped2);
+        // for (const auto[edge_indices, weights] : hypercube) {
             
-        }
+        // }
+        return 0;
     };
     std::vector<std::unique_ptr<Container<T>>> m_Grid;
     xt::xarray<T> m_Data;
+    size_t m_NDIMS;
 };
 
 #endif
--- tests/test_interpolator.cpp
+++ tests/test_interpolator.cpp
@@ -2,54 +2,88 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <xtensor/xarray.hpp>
+#include <xtensor/xbuilder.hpp>
 #include <xtensor/xnpy.hpp>
 
 TEST(Interpolator, Regular1D)
 {
-    double min = 0;
-    double max = 3;
-    int n = 3;
-    auto xarr = xt::linspace<double>(min, max, n);
-    std::unique_ptr<Container<double>> xcoord = std::make_unique<Regular<double>>(min, max, n);
-    auto yarr = xt::eval(2*xarr);
-    auto xin = xt::linspace(min, max, n*3);
-    std::vector<std::unique_ptr<Container<double>>> grid;
-    grid.push_back(std::move(xcoord));
-    Interpolator<double> interp(std::move(grid), yarr);
-    auto interpolator_res = interp(xin);
+    // double min = 0;
+    // double max = 3;
+    // int n = 3;
+    // auto xarr = xt::linspace<double>(min, max, n);
+    // std::unique_ptr<Container<double>> xcoord = std::make_unique<Regular<double>>(min, max, n);
+    // auto yarr = xt::eval(2*xarr);
+    // auto xin = xt::linspace(min, max, n*3);
+    // std::vector<std::unique_ptr<Container<double>>> grid;
+    // grid.push_back(std::move(xcoord));
+    // Interpolator<double> interp(std::move(grid), yarr);
+    // auto interpolator_res = interp(xin);
 
-    auto xt_res = xt::eval(xt::interp(xin, xarr, yarr));
+    // auto xt_res = xt::eval(xt::interp(xin, xarr, yarr));
 
-    ASSERT_EQ(xt_res.size(), interpolator_res.size());
-    ASSERT_EQ(interpolator_res.size(), xin.size());
+    // ASSERT_EQ(xt_res.size(), interpolator_res.size());
+    // ASSERT_EQ(interpolator_res.size(), xin.size());
 
-    for (size_t i=0; i<interpolator_res.size(); ++i) {
-        EXPECT_EQ(interpolator_res[i], xt_res[i]);
-    }
+    // for (size_t i=0; i<interpolator_res.size(); ++i) {
+    //     EXPECT_EQ(interpolator_res[i], xt_res[i]);
+    // }
 }
 
 TEST(Interpolator, Regular2D)
 {
-    size_t m = 10;
-    size_t n = 20;
+    // size_t m = 10;
+    // size_t n = 20;
+    // std::vector<std::unique_ptr<Container<double>>> grid;
+    // grid.push_back(std::move(std::make_unique<Regular<double>>(0, 10, m)));
+    // grid.push_back(std::move(std::make_unique<Regular<double>>(0, 10, n)));
+
+    // xt::xarray<double> x = xt::linspace<double>(0, 10, m);
+    // xt::xarray<double> y = xt::linspace<double>(0, 10, n);
+
+    // x.reshape({m, 1});
+    // y.reshape({1, n});
+
+    // auto data = xt::eval(x*y);
+    // data.reshape({m, n});
+
+    // Interpolator<double> interp(std::move(grid), data);
+
+    // // xt::xarray<double> a0 = xt::linspace(0, 10, 15);
+    // // xt::xarray<double> b0 = xt::linspace(0, 10, 15);
+    // // auto xin = xt::vstack(xt::xtuple(a0, b0));
+
+    // xt::xarray<double> xin2 = {
+    //     {2.1, 6.2, 8.3},
+    //     {3.3, 5.2, 7.1}
+    // };
+
+    // interp(xin2);
+}
+
+auto f(xt::xarray<double> x, xt::xarray<double> y, xt::xarray<double> z) {
+    return 2 * xt::pow(x, 3) + 3 * xt::pow(y, 2) - z;
+}
+
+TEST(Interpolator, Regular3D)
+{
     std::vector<std::unique_ptr<Container<double>>> grid;
-    grid.push_back(std::move(std::make_unique<Regular<double>>(0, 10, m)));
-    grid.push_back(std::move(std::make_unique<Regular<double>>(0, 10, n)));
+    grid.emplace_back(new Regular<double>(1, 4, 11));
+    grid.emplace_back(new Regular<double>(4, 7, 22));
+    grid.emplace_back(new Regular<double>(7, 9, 33));
 
-    xt::xarray<double> x = xt::linspace<double>(0, 10, m);
-    xt::xarray<double> y = xt::linspace<double>(0, 10, n);
+    auto x = xt::linspace<double>(1, 4, 11);
+    auto y = xt::linspace<double>(4, 7, 22);
+    auto z = xt::linspace<double>(7, 9, 33);
 
-    x.reshape({m, 1});
-    y.reshape({1, n});
+    auto [xg, yg, zg] = xt::meshgrid(x, y, z);
+    auto data = xt::eval(f(xg, yg, zg));
 
-    auto data = xt::eval(x*y);
-    data.reshape({m, n});
+    Interpolator interp(std::move(grid), data);
 
-    Interpolator<double> interp(std::move(grid), data);
+    xt::xarray<double> res = {
+        {2.1, 6.2, 8.3},
+        {3.3, 5.2, 7.1}
+    };
 
-    xt::xarray<double> a0 = xt::linspace(0, 10, 15);
-    xt::xarray<double> b0 = xt::linspace(0, 10, 15);
-    auto xin = xt::vstack(xt::xtuple(a0, b0));
-
-    interp(xin);
+    auto rtn = interp(res);
 }
